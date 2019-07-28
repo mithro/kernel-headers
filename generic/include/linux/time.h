734e216bac94c7e59b249fc657525d1874937652
Tim 'mithro' Ansell <me@mith.ro>
2019-07-27 20:29:13 -0400

    Updating to Linux Kernel 5.2.3

--- generic/include/linux/time.h
+++ generic/include/linux/time.h
@@ -1,34 +1,27 @@
+/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
 #ifndef _LINUX_TIME_H
 #define _LINUX_TIME_H
 
 #include <linux/types.h>
-#include <linux/libc-compat.h>
-
+#include <linux/time_types.h>
 
 #ifndef _STRUCT_TIMESPEC
 #define _STRUCT_TIMESPEC
-#if __UAPI_DEF_TIMESPEC
 struct timespec {
 	__kernel_time_t	tv_sec;			/* seconds */
 	long		tv_nsec;		/* nanoseconds */
 };
 #endif
-#endif
 
-#if __UAPI_DEF_TIMEVAL
 struct timeval {
 	__kernel_time_t		tv_sec;		/* seconds */
 	__kernel_suseconds_t	tv_usec;	/* microseconds */
 };
-#endif
 
-#if __UAPI_DEF_TIMEZONE
 struct timezone {
 	int	tz_minuteswest;	/* minutes west of Greenwich */
 	int	tz_dsttime;	/* type of dst correction */
 };
-#endif
-
 
 /*
  * Names of the interval timers, and structure
@@ -38,19 +31,15 @@ struct timezone {
 #define	ITIMER_VIRTUAL		1
 #define	ITIMER_PROF		2
 
-#if __UAPI_DEF_ITIMERSPEC
 struct itimerspec {
 	struct timespec it_interval;	/* timer period */
 	struct timespec it_value;	/* timer expiration */
 };
-#endif
 
-#if __UAPI_DEF_ITIMERVAL
 struct itimerval {
 	struct timeval it_interval;	/* timer interval */
 	struct timeval it_value;	/* current value */
 };
-#endif
 
 /*
  * The IDs of the various system clocks (for POSIX.1b interval timers):
@@ -65,7 +54,11 @@ struct itimerval {
 #define CLOCK_BOOTTIME			7
 #define CLOCK_REALTIME_ALARM		8
 #define CLOCK_BOOTTIME_ALARM		9
-#define CLOCK_SGI_CYCLE			10	/* Hardware specific */
+/*
+ * The driver implementing this got removed. The clock ID is kept as a
+ * place holder. Do not reuse!
+ */
+#define CLOCK_SGI_CYCLE			10
 #define CLOCK_TAI			11
 
 #define MAX_CLOCKS			16
@@ -75,8 +68,6 @@ struct itimerval {
 /*
  * The various flags for setting POSIX.1b interval timers:
  */
-#ifndef TIMER_ABSTIME
 #define TIMER_ABSTIME			0x01
-#endif
 
 #endif /* _LINUX_TIME_H */
